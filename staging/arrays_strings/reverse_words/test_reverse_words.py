fa63bf5cd34bd35b7a9a4ce7b8e7d918e45cf327
Michael T <bravmech@gmail.com>
2020-04-29 09:23:36 +0500

    reverse words done

--- staging/arrays_strings/reverse_words/test_reverse_words.py
+++ staging/arrays_strings/reverse_words/test_reverse_words.py
@@ -1,6 +1,15 @@
 from nose.tools import assert_equal
+import import_ipynb
+from .reverse_words_challenge import reverse_words
+import pytest
 
-class UnitTest (object):
+
+@pytest.fixture
+def func():
+    return lambda S: reverse_words(S)
+
+
+class Test (object):
     def testReverseWords(self, func):
             assert_equal(func('the sun is hot'), 'eht nus si toh')
             assert_equal(func(''), None)
@@ -9,7 +18,7 @@ class UnitTest (object):
             print('Success: reverse_words')
             
 def main():
-    test = UnitTest()
+    test = Test()
     test.testReverseWords(reverse_words)
 
 if __name__=="__main__":
